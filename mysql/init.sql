8cbaf5e846cf2a91000a41b0a012201297689555
YangSeungwon <yysw1109@gmail.com>
2023-02-20 07:25:53 +0000

    setup

---
+++ mysql/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS restaurants (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    count INT UNSIGNED NOT NULL,
+    score FLOAT NOT NULL,
+    last_changed TIMESTAMP
+);
+
+
+
+SET collation_connection = 'utf8_general_ci';
+ALTER DATABASE app CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE restaurants CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
