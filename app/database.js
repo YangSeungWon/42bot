567183a75b02069f24cd03cc6973e0d6980cd881
YangSeungwon <yysw1109@gmail.com>
2023-03-02 08:04:37 +0000

    select with descending order

--- app/database.js
+++ app/database.js
@@ -33,7 +33,7 @@ class Database {
       sql += ' WHERE ' + keys.map(key => `${key} = ?`).join(' AND ');
     }
     if (orderBy) {
-      sql += ` ORDER BY ${mysql.escapeId(orderBy)}`;
+      sql += ` ORDER BY ${mysql.escapeId(orderBy)} DESC`;
     }
     if (limit_num) {
       sql += ` LIMIT ${limit_from}, ${limit_num}`;
